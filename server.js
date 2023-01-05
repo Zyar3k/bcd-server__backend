7fe3574b8f4abea12298fdc8d54a8b103f5cc07a
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-05 20:05:23 +0100

    Init project

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
 
-const express = require("express");
-
-const app = express();
 const PORT = process.env.PORT || 8888;
+const app = express();
 
-const start = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Server started: http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-start();
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome on server!</h1>");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(console.log("api products"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

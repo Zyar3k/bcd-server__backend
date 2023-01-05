95d43b4d207da8463263e156e931f6ef8f47919f
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-05 20:18:26 +0100

    Refactor server

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ import express from "express";
 import cors from "cors";
 
 const PORT = process.env.PORT || 8888;
+import productRoutes from "./routes/product.routes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/api", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome on server!</h1>");
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(console.log("api products"));
 });
 
 app.listen(PORT, () => {
